5d6fd5cd644dc930fec78148388488ccdf5e6e84
skojphy <skojphy@gmail.com>
2021-10-01 16:09:56 +0900

    feat: Create countCompletedTodos function

---
+++ arrayHof8.js
@@ -0,0 +1,10 @@
+const todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+const countCompletedTodos = todos =>
+  todos.filter(item => item.completed).length;
+
+console.log(countCompletedTodos(todos)); // 1
