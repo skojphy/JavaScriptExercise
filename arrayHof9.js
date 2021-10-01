7f79839c4476577bd57aeffd85192e322773acf6
skojphy <skojphy@gmail.com>
2021-10-01 16:18:08 +0900

    feat: Create getMaxId function

--- arrayHof9.js
+++ arrayHof9.js
@@ -4,7 +4,7 @@ const todos = [
   { id: 1, content: 'Javascript', completed: false }
 ];
 
-const getMaxId = todos => Math.max(todos.map(item => item.id));
+const getMaxId = todos => Math.max(...todos.map(item => item.id));
 
 console.log(getMaxId(todos)); // 3
 console.log(getMaxId([])); // 0
