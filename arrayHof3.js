31b8ca7fba9e7bdc220bb4637b63b62fa9e32550
skojphy <skojphy@gmail.com>
2021-10-01 16:02:12 +0900

    feat: Create sortBy, addTodo, removeTodo functions

---
+++ arrayHof3.js
@@ -0,0 +1,33 @@
+let todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+const sortBy = (todos, key) => todos.sort((a, b) => a[key] - b[key]);
+
+todos = sortBy(todos, 'id');
+console.log(todos);
+/*
+  [
+    { id: 1, content: 'Javascript', completed: false },
+    { id: 2, content: 'CSS', completed: true },
+    { id: 3, content: 'HTML', completed: false }
+  ]
+  */
+console.log(sortBy(todos, 'content'));
+/*
+  [
+    { id: 2, content: 'CSS', completed: true },
+    { id: 3, content: 'HTML', completed: false },
+    { id: 1, content: 'Javascript', completed: false }
+  ]
+  */
+console.log(sortBy(todos, 'completed'));
+/*
+  [
+    { id: 3, content: 'HTML', completed: false },
+    { id: 1, content: 'Javascript', completed: false },
+    { id: 2, content: 'CSS', completed: true }
+  ]
+  */
