1955481874386499163b86c16ffb465b2ecb0159
skojphy <skojphy@gmail.com>
2021-10-01 16:40:13 +0900

    refactor: Refactor getValues function

--- arrayHof2.js
+++ arrayHof2.js
@@ -4,7 +4,7 @@ const todos = [
   { id: 1, content: 'Javascript', completed: false }
 ];
 
-const getValues = (todos, key) => todos.map(item => item[key]);
+const getValues = (todos, key) => todos.map(todo => todo[key]);
 
 console.log(getValues(todos, 'id')); // [3, 2, 1]
 console.log(getValues(todos, 'content')); // ['HTML', 'CSS', 'Javascript']
